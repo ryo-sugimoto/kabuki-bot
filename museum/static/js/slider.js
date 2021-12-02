3a3503c1a3fdb4ad62842408732d3b289861c76b
Ryo <test@test.com>
2021-12-02 15:24:38 +0000

    ryo1

---
+++ museum/static/js/slider.js
@@ -0,0 +1,25 @@
+$('.slider').slick({
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    prevArrow: '<div class="slick-prev"></div>',
+    nextArrow: '<div class="slick-next"></div>',
+    dots: true,
+    responsive: [
+        {
+        breakpoint: 769,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+        }
+    },
+    {
+        breakpoint: 426,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        }
+    }
+]
+  });
